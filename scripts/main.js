6b4545df7af3648be5147187d6d37f63ed9fea4f
Corbee_Rian <rianchamarelli@gmail.com>
2022-11-03 17:04:27 -0300

    Atualizando os modulos novamente

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+import tocaSom from "./modules/tocaSom.js";
+    
+    const teclas = document.querySelectorAll('.tecla');
+    
+    const reproduzSom = teclas.forEach(element => {
+        const instrumento = element.classList[1];
+        const idAudio = (`#som_${instrumento}`);
+         
+        element.addEventListener('click', () => {
+            tocaSom(idAudio);
+        })
+    })   
+
