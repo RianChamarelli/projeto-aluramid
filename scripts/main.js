6248bf0f18cb95cc7c3521b179b9e5ee7e1df028
Corbee_Rian <rianchamarelli@gmail.com>
2022-11-04 07:44:00 -0300

    Eventos de teclado foram adicionados

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,24 @@ import tocaSom from "./modules/tocaSom.js";
     
     const reproduzSom = teclas.forEach(element => {
         const instrumento = element.classList[1];
-        const idAudio = (`#som_${instrumento}`);
+        const idAudio = (`#som_${instrumento}`);        
          
         element.addEventListener('click', () => {
             tocaSom(idAudio);
         })
+
+        element.addEventListener('keydown', (event) => {
+            if (event.code === 'Space' || event.code === 'Enter'){
+                element.classList.add('ativa');
+            }   
+        }) 
+
+        element.addEventListener('keyup', () => {
+            element.classList.remove('ativa');
+        })
+
+
     })   
 
+
+/home
